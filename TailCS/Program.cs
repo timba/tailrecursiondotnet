454b1b3c5a3d22f5b693fdb8ec38197fff7d4d52
Timur Babyuk <tba@kyiv.ciklum.net>
2016-12-25 12:42:28 +0200

    Initial commit

---
+++ TailCS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TailCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Recursive(0));
+        }
+
+        public static int Recursive(int i)
+        {
+            if (i % 1000 == 0)
+            {
+                Console.WriteLine(i);
+            }
+
+            return Recursive(i + 1);
+        }
+    }
+}
---
+++ TailFS/Program.fs
@@ -0,0 +1,10 @@
+﻿let rec Recursive i =
+    if (i % 1000 = 0) then
+        System.Console.WriteLine(i)
+
+    Recursive (i + 1)
+
+[<EntryPoint>]
+let main argv = 
+    Recursive 0
+    0
